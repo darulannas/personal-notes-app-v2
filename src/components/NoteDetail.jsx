69db754fa4b9c5f73718adc0af8b0a8df6257e09
darulannas <darulannas7@gmail.com>
2023-09-08 15:38:59 +0700

    fix NoteDetail

--- src/components/NoteDetail.jsx
+++ src/components/NoteDetail.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import DeleteButton from "./DeleteButton";
 import ArchiveButton from "./ArchiveButton";
+import { showFormattedDate } from "../utils/index";
 
 function NoteDetail({ title, createdAt, body, id, onDelete, onArchive, archived }) {
   return (
     <div className="detail-page">
       <h3 className="detail-page__title">{title}</h3>
-      <p className="detail-page__createdAt">{createdAt}</p>
+      <p className="detail-page__createdAt">{showFormattedDate(createdAt)}</p>
       <p className="detail-page__body">{body}</p>
       <div className="detail-page__action">
         <ArchiveButton id={id} onArchive={onArchive} archived={archived} />
